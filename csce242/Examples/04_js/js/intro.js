0d0e9b7d8d6a393342349ebb189707a8ac04329e
Samuelbsk8 <Sammybsk8@gmail.com>
2025-09-10 10:36:47 -0400

    wireframe

---
+++ csce242/Examples/04_js/js/intro.js
@@ -0,0 +1,11 @@
+/*
+const sayHello = () => {
+    console.log("Hellow World");
+}
+
+document.getElementById("btn-click-me").onclick = sayHello;
+*/
+
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("hello world");
+};
